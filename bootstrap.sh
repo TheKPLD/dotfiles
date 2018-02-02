640f9f0e053e98335e033ddd6d0da3816174b39e
Kyle Dion <kdion@twilio.com>
2018-02-02 11:57:26 -0800

    gitconfig from my laptop, bootstrap.sh updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
+set -e
 
-cd "$(dirname "${BASH_SOURCE}")";
+cd "$(dirname "${BASH_SOURCE}")"
 
-git pull origin master;
+git pull -q origin master
 
-function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
-	source ~/.bash_profile;
+function rsync_cmd() {
+	rsync $1 --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude '.gitignore' \
+        -avh --no-perms . ~
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+	rsync_cmd
+    # running source ~/.bash_profile with -e currently errors out
+	source ~/.bash_profile || :
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
+    rsync_cmd '--dry-run'
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo ""
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+		rsync_cmd
+	fi
+fi
+unset rsync_cmd
