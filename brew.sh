b1580cb31abda6c92a3db44df1daea388badb8f0
nobody@no.where <nobody@no.where>
2018-03-13 14:22:36 -0700

    brew.sh installs homebrew

--- brew.sh
+++ brew.sh
@@ -5,6 +5,8 @@
 # Ask for the administrator password upfront.
 sudo -v
 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
