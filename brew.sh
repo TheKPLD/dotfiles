98f1a893e6c8fa497dcfb506014d1f5522e606bd
nobody@no.where <nobody@no.where>
2018-03-13 14:50:57 -0700

    only install homebrew if it can't be found

--- brew.sh
+++ brew.sh
@@ -5,7 +5,11 @@
 # Ask for the administrator password upfront.
 sudo -v
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if ! brew --help>>/dev/null;
+then
+    echo "Installing HomeBrew!"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
